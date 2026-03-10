e97ad3de66b58f454878e04955ca8e79658d85f8
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2026-03-10 18:53:47 -0400

    prisma schema,

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import "dotenv/config";
 import { authRouter, indexRouter } from "./src/routes/index.routes.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Listening on http://localhost:3000");
 });
---
+++ prisma/migrations/20260310215425_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ownerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "folderId" INTEGER NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Folder_name_ownerId_key" ON "Folder"("name", "ownerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_name_folderId_key" ON "File"("name", "folderId");
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
