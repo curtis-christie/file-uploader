bfefc38ed334b8a2125fa0e5e7952e1e02103cdf
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2026-03-10 16:46:19 -0400

    restart

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-import "dotenv/config";
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { indexController } from "../controllers/index.controller.js";
-
-const usersRouter = Router();
-
-usersRouter.get("/signup", (req, res) => res.render("signup"));
-
-export default usersRouter;
